262f2aae6ad46b0e6c5108de57f9302ce23a1e37
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-16 11:26:21 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,26 +11,15 @@ class Config():
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "5871159415:AAEmdLPpLY_8maR5xPCMCR3GIVg9-tFBqVs"
+    DATABASE_URL = "postgresql://wlsokvlw:iXaEM2r2BjhK5GcOXGm2qJWotsCjChR0@floppy.db.elephantsql.com/wlsokvlw"
+    APP_ID = "24515526"
+    API_HASH = "482182f8ab9dd82fb9144a0f68eeb817"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
-    SUDO_USERS.append(939425014)
+    SUDO_USERS.append(1947738686)
     SUDO_USERS = list(set(SUDO_USERS))
 
 
 class Messages():
-      HELP_MSG = [
-        ".",
 
-        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
-        
-        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
-        
-        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
-        
-        "**Developed by @viperadnan**"
-      ]
-
-      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
+      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /start__"
